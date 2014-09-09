d4816b01b2a8deb60c9cafad417be98970ed752e
Ivan Deviatkin <devyatkin.ivan@gmail.com>
2014-09-09 21:32:17 +0200

    Initial version of project.

---
+++ inc/algebra_elementary.hpp
@@ -0,0 +1,67 @@
+/*
+*
+*
+*
+*/
+
+#pragma once
+
+#include <algorithm>
+
+using namespace std;
+
+namespace Fudzi
+{
+
+class AlgebraElementary
+{
+private:
+protected:
+public:
+	// @brief Euler function calculation
+	// @param n natural upper border
+	// @return total amount of simple numbers with n in range [0;n)
+	int phi(int n);
+	// @brief Binary power a^n
+	// @param a number
+	// @param n power
+	// @return value a^n
+	int binpow(int a, int n);
+	// @brief Binary power a^n faster version
+	// @param a number
+	// @param n power
+	// @return value a^n
+	int binpowEx(int a, int n);
+	// @brief Greatest common divisor, Euklid algorithm (recursive)
+	// @param a first number
+	// @param b second number
+	// @return gcd for a and b
+	int gcdRecursive(int a, int b);
+	// @brief Greatest common divisor, Euklid algorithm, faster version
+	// @param a first number
+	// @param b second number
+	// @return gcd for a and b
+	int gcdNonRecursive(int a, int b);
+	// @brief Greatest common divisor, Euklid algorithm, faster version
+	// @param a first number
+	// @param b second number
+	// @param x coefficient for first number
+	// @param y coefficient for second number
+	// @return gcd for a and b
+	int gcdExtended(int a, int b, int &x, int &y);
+	// @brief Lowest common multplier
+	// @param a first number
+	// @param b second number
+	// @return lcm for a and b
+	int lcm(int a, int b);
+	// @brief Searches all simple numbers in range [1;n]
+	// @param n right border of alg
+	// @return vector of simple marks in range [1;n]
+	vector<char> sieveOfEratosthenes(int n);
+	// @brief Fibonacchi number of n index
+	// @param n index of Fibonacchi number
+	// @return Fibonacchi number of n index
+	int fibNumberBinet(int n);
+
+};
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+2014-09-05
+This library collects algorithms based on description from e-maxx.ru
+Thanks to Maksim Ivanov
