b4afb33379a769602521c8760190c17dd9ab9eae
Volkodavchik <devyatkin.ivan@gmail.com>
2015-03-02 16:42:09 +0100

    1. Updated task 2 about friends network

--- tasks/task2/main.cpp
+++ tasks/task2/main.cpp
@@ -6,27 +6,57 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <queue>
+#include <vector>
+#include <tuple>
 // Fudzi
 #include "algebra_elementary.hpp"
 #include "strings.hpp"
 
 using namespace std;
 
-void parseGivenString(string input)
+void insert_or_assign(string person, vector<string> friends, std::map <string, vector<string>> & network)
+{
+    auto search = network.find(person);
+
+    if (search != network.end())
+    {
+        //std::cout << "Found " << search->first << '\n';
+        if (!friends.empty())
+        {
+            //add all elements
+            for (auto fr : friends)
+            {
+                search->second.push_back(fr);
+            }
+        }
+    }
+    else
+    {
+        //std::cout << "Not found " << person << " lets add it \n";
+        network.insert(std::make_pair(person, friends));
+    }
+}
+
+void parseGivenString(string input, std::map <string, vector<string>> & network)
 {
     string head;
+    vector<string> friends;
     string temp;
     for (auto el : input)
     {
         if(el == ':')
         {
             head = temp;
-            cout << "head friend: " << head << endl;
+            std::cout << "head "<< head << std::endl;
             temp.erase();
         }
         else if( el == ',')
         {
-            cout << "next friend: " << temp << endl;
+            friends.push_back(temp);
+            std::cout << "friend "<< temp << std::endl;
+            insert_or_assign(temp, vector<string>(), network);
             temp.erase();
         }
         else
@@ -36,17 +66,77 @@ void parseGivenString(string input)
     }
     if (!temp.empty())
     {
-        cout << "next friend: " << temp << endl;
+        std::cout << "friend "<< temp << std::endl;
+        insert_or_assign(temp, vector<string>(), network);
+        friends.push_back(temp);
         temp.erase();
+    }
+    insert_or_assign(head, friends, network);
+}
+
+void search_wide(std::map <string, vector<string>> & g, string start)
+{
+    vector<string> res;
+
+    std::map <string, bool> used;
+    for (auto el : g)
+    {
+        used.insert(make_pair(el.first, false));
+    }
+    used[start] = true;
+    //
+    queue<string> q;
+    q.push (start);
+
+    while (!q.empty())
+    {
+        auto v = q.front();
+        q.pop();
+        string friends;
+        for (auto el : g[v])
+        {
+            if (!used[el])
+            {
+                used[el] = true;
+                q.push(el);
+                friends += el + ",";
+            }
+        }
+        if (!friends.empty())
+        {
+            string out = v + ": " + friends;
+            std::cout << out << endl;
+            res.push_back(out);
+        }
     }
 }
 
 int main()
 {
-    string input = "A:B,C,D";
+    vector<string> input;
+    input.push_back("A:B,C,D");
+    input.push_back("B:A,D");
+    input.push_back("C:B,E");
 
-    cout << "given string: " << input << endl;
-
+    // create graph
+    std::map <string, vector<string>> network;
+    for(const auto &el : input)
+    {
+        parseGivenString(el, network);
+        cout << endl;
+    }
+    // check of graph
+    for (const auto &p : network)
+    {
+        auto friends = p.second;
+        cout << p.first << " has " << friends.size() << " friends:" << endl;
+        for (const auto &el : p.second)
+        {
+            cout << el << endl;
+        }
+    }
+    // search for all friends from start point
+    search_wide(network, "A");
 
     return 0;
 }
