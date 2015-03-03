172e63502b23476a3eab5851c722330a8c345bc0
Volkodavchik <devyatkin.ivan@gmail.com>
2015-03-03 14:38:34 +0100

    1. Removed static methods from strings.hpp

--- inc/strings.hpp
+++ inc/strings.hpp
@@ -26,26 +26,26 @@ public:
 	//! @brief Efficiency algorithm for getting zet function for string
 	//! @param s string
 	//! @return vector of zet function for given string
-	static std::vector<int> z_function(std::string s);
+	std::vector<int> z_function(std::string s);
 	//! @brief Trivial algorithm for getting prefix function for string
 	//! @param s string
 	//! @return vector of prefix function for given string
-	static std::vector<int> prefix_function_trivial(std::string s);
+	std::vector<int> prefix_function_trivial(std::string s);
 	//! @brief Trivial algorithm for getting prefix function for string
 	//! @param s string
 	//! @return vector of prefix function for given string
-	static std::vector<int> prefix_function(std::string s);
+	std::vector<int> prefix_function(std::string s);
 	//! @brief Calculates hash for string with using provided weight coefficient
 	//! @param s string
 	//! @param weightCoef coefficient for calculating hash, default value 31
 	//! @return hash for provided string
-	static long long hash_strung(std::string s, long long weightCoef = 31);
+	long long hash_strung(std::string s, long long weightCoef = 31);
 	//! @brief Searches and returns all substrings in given string(text)
 	//! @param substring string for looking
 	//! @param text given text in which we are looking for a substring
 	//! @param divider symbol which is not present in text/substring
 	//! @return number of substrings in given string
-	static unsigned SearchSubstringInText(std::string substring, std::string text, std::string divider);
+	unsigned SearchSubstringInText(std::string substring, std::string text, std::string divider);
 	//! @brief Non-recursive algorithm for unordered generation of permutations.
 	//! For the string s of length n, for any k from 0 to n! - 1 inclusive, the following
 	//! modifies s to provide a unique permutation (that is, different from those generated for
@@ -53,11 +53,11 @@ public:
 	//! values on the original value of s.Searches and returns all substrings in given string(text)
 	//! @param k value for permutation alg
 	//! @param s string for generating new permutation
-	static void permutationUnordered(int k, std::string &s);
+	void permutationUnordered(int k, std::string &s);
 	//! @brief Reverse string with reverse iterator
 	//! @param input string for revrse
 	//! @return reversed string
-	static std::string reverseWithSTL(std::string &input);
+	std::string reverseWithSTL(std::string &input);
 
 
 };
